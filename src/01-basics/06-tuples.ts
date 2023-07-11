12e99dfeb4258b0ed05af997e1fab6b2704f3db4
Stas <46508483+Stas9iMba@users.noreply.github.com>
2023-07-12 04:05:15 +0700

    tuples

---
+++ src/01-basics/06-tuples.ts
@@ -0,0 +1,13 @@
+// кортеж
+const pairs: [string, string][] = [
+  ["key", "value"],
+  ["key2", "value2"],
+  ["key3", "value3"],
+];
+
+const data: [number, boolean, string] = [1, true, "lodash"];
+
+// csv
+const doc: [string, string, number, Date][] = [];
+doc.push(["John", "Doe", 30, new Date(1994, 1, 1)]);
+
